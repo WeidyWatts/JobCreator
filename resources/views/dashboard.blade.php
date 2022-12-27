3826e09650a84a6d3acef3a4a26a4a8a2ac6b901
victor.nunes <victor.nunes@agu.gov.br>
2022-12-27 18:11:15 -0300

    [FIX] ajuste FAQ

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -83,10 +83,99 @@ z-index:900;">
             </div>
             <div class="modal-body">
 
-                <embed src="{{asset('img/slides/guia-acesso.pdf')}}"
-                       width="100%"
-                       height="700em">
-            </div>
+                <div class="d-flex justify-content-center mb-5">
+                    <div class="titulo faq-titulo">
+                        <b>GUIA DE ACESSO DA PLATAFORMA JOBCREATORS</b>
+                    </div>
+                </div>
+
+                <div class="d-flex justify-content-start mt-5">
+                    <div class="faq-sub">
+                        <b>Como acessar a plataforma?</b>
+                    </div>
+                </div>
+
+
+                <div class="d-flex justify-content-start mb-5 mt-2">
+                    <div class="faq-text">
+                        <i class="fa fa-arrow-right" ></i> <b class="orange">Você receberá um email</b>  te convidando para realizar o primeiro login na
+                        plataforma (não esqueça de verificar o seu spam, ok?).
+                    </div>
+                </div>
+
+                <div class="d-flex justify-content-start mb-5 mt-2">
+                    <div class="faq-text">
+                        <i class="fa fa-arrow-right" ></i> <b class="orange"> É importante reforçar,</b>  não tente logar na plataforma com um endereço de email diferente, para você ter acesso, o email de login deve ser igual ao qual foi enviado o
+                        convite de acesso a JobCreators.
+                    </div>
+                </div>
+
+                <div class="d-flex justify-content-start mb-5 mt-2">
+                    <div class="faq-text">
+                        <i class="fa fa-arrow-right" ></i> Após o primeiro login, você pode acessar o nosso site <a class="orange-color" href="https://jobcreators.work/login"> https://jobcreators.work/</a> e clicar em “login” para ter acesso a sua jornada.
+                    </div>
+                </div>
+
+                <div class="d-flex justify-content-start mt-5">
+                    <div class="faq-sub">
+                        <b>Como acessar as videoaulas da minha jornada?</b>
+                    </div>
+                </div>
+
+
+
+                <div class="d-flex justify-content-start mb-5 mt-2">
+                    <div class="faq-text">
+                        <i class="fa fa-arrow-right" ></i>  Você poderá acessar as videoaulas de 3 formas, mas é importante reforçar que para avançar nos módulos, você deve assistir no mínimo 90% de cada conteúdo.
+
+                    </div>
+                </div>
+
+                <div class="d-flex justify-content-start mb-5 mt-2">
+                    <div class="faq-text">
+                        <b class="white-b-orange">1.</b> Após realizar o seu login, aparecerá uma tela para que você já possa iniciar a sua
+                        jornada ou então retomar do ponto em que parou desde o seu último acesso.
+                    </div>
+                </div>
+
+
+                <div class="d-flex justify-content-start mb-5 mt-2">
+                    <div class="faq-text">
+                        <b class="white-b-orange">2.</b>  Ao fechar a tela anterior, você pode acessar a sua jornada novamente através do
+                        caminho: <b class="orange">Minha Journey -> Minha Carreira -> Entrar na Journey</b>
+                    </div>
+                </div>
+
+
+                <div class="d-flex justify-content-start mb-5 mt-2">
+                    <div class="faq-text">
+                        <b class="white-b-orange">3.</b> Após entrar na Journey, você terá acesso a sua jornada.
+                    </div>
+                </div>
+
+
+                <div class="d-flex justify-content-start mb-5 mt-2">
+                    <div class="faq-text">
+                        <b class="white-b-orange">4.</b> Ao fechar a tela de jornada inicial, você pode acessar a sua jornada novamente através
+                        do botão localizado ao lado direito da sua tela inicial. Após clicar em Offboarding, você
+                        deverá clicar em continuar para retomar ou iniciar a sua jornada.
+                    </div>
+                </div>
+
+
+                <div class="d-flex justify-content-start mt-5">
+                    <div class="faq-sub">
+                        <b>Como acessar os materiais de apoio?</b>
+                    </div>
+                </div>
+
+                <div class="d-flex justify-content-start mb-5 mt-2">
+                    <div class="faq-text">
+                        Os materiais de apoio estão disponíveis dentro da plataforma e você poderá acessá-los a qualquer momento durante a sua jornada. Além de acessar os materiais, você pode baixá-los para utilizar durante o seu processo de recolocação profissional.
+
+                    </div>
+                </div>
+                
             <div class="modal-footer">
                 <button type="button" class="btn salvar" data-bs-dismiss="modal">OK </button>
             </div>
